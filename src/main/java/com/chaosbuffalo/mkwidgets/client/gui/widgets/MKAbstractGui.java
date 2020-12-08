0a33d9d1f8e4268ec59c272019c95f3f11736211
Kovak <kovac1066@gmail.com>
2020-12-08 01:06:49 -0800

    remove the inner blit funcs

--- src/main/java/com/chaosbuffalo/mkwidgets/client/gui/widgets/MKAbstractGui.java
+++ src/main/java/com/chaosbuffalo/mkwidgets/client/gui/widgets/MKAbstractGui.java
@@ -18,19 +18,4 @@ public abstract class MKAbstractGui extends AbstractGui {
     public static void mkFill(MatrixStack matrixStack, int x0, int y0, int x1, int y1, int color){
         fill(matrixStack, x0, y0, x1, y1, color);
     }
-
-//    public static void mkInnerBlit(MatrixStack matrixStack, int x0, int x1, int y0, int y1,
-//                                   int z, int uSize, int vSize, float u0, float v0,
-//                                   int texWidth, int texHeight) {
-//        mkInnerBlit(matrixStack, x0, x1, y0, y1,
-//                z,
-//                u0 / (float)texWidth,
-//                (u0 + (float)uSize) / (float)texWidth,
-//                v0 / (float)texHeight, (v0 + (float)vSize) / (float)texHeight);
-//    }
-//
-//    public static void mkInnerBlit(MatrixStack matrixStack, int x0, int x1, int y0, int y1, int z,
-//                                   float u0, float u1, float v0, float v1) {
-//        innerBlit(matrixStack, x0, x1, y0, y1, z, u0, u1, v0, v1);
-//    }
 }
